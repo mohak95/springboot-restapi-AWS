c095ed96031424199a84216c009c1603168315df
dell <dell@DESKTOP-VA4E2KI>
2021-09-12 16:53:53 +0530

    second Commit

--- springboot-crud/src/main/java/com/javaguides/springbootcrud/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.javaguides.springbootcrud.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.javaguides.springbootcrud.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Integer>{
-
-}
